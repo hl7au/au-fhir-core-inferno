4e5b186d612d36a676d0c3fc4501a8cdf8e86366
Pavel R <prozskov@gmail.com>
2024-03-13 15:35:02 +0100

    Refactor MDSummaryGenerator Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/4

--- lib/au_core_test_kit/generator/mdsummary_generator.rb
+++ lib/au_core_test_kit/generator/mdsummary_generator.rb
@@ -2,6 +2,9 @@ require_relative 'naming'
 require_relative 'special_cases'
 require_relative '../helpers'
 
+# Resources that we don't need to show in the summary exhibit behavior similar to that of the generated test groups.
+EXCLUDED_RESOURCES = %w[Location Medication PractitionerRole]
+
 module AUCoreTestKit
   class Generator
     class MDSummaryGenerator
@@ -38,62 +41,69 @@ module AUCoreTestKit
         File.open(output_file_name, 'w') { |f| f.write(output) }
       end
 
-      def resource_list
-        groups = ig_metadata.ordered_groups.reject { |group| ["Location", "Medication", "PractitionerRole"].include? group.resource }
-        mapped_groups = groups.map.with_index do |group, index|
-          title = group.title
-          resource_type = group.resource
-          profile_name = group.profile_name
-          profile_url = group.profile_url
-          required_searches = group.searches.select { |search| search[:expectation] == 'SHALL' }
-          search_param_name_string = required_searches
-                                       .map { |search| search[:names].join(' + ') }
-                                       .map { |names| "* #{names}" }
-                                       .join("\n")
-          search_validation_resource_type = "#{resource_type} resources"
-          group_description = Helpers.get_group_description_text(title,
-                                                                 resource_type,
-                                                                 profile_name,
-                                                                 group.version,
-                                                                 profile_url,
-                                                                 required_searches,
-                                                                 search_param_name_string,
-                                                                 search_validation_resource_type,
-                                                                 false)
+      def prepare_interactions(interactions)
+        interactions.map do |interaction|
           {
-            'title' => title,
-            'resource' => resource_type,
-            'position' => index + 2,
-            'profile_url' => profile_url,
-            'description' => group_description,
-            'interactions' => group.interactions.map do |interaction|
-              {
-                'code' => interaction[:code],
-                'expectation' => interaction[:expectation],
-              }
-            end,
-            'searches' => group.searches.map.with_index do |search, index|
-              {
-                'names' => search[:names],
-                'expectation' => search[:expectation],
-                'search_string' => make_search_string(group.resource, search[:names]),
-                'position' => index + 1
-              }
-            end
+            'code' => interaction[:code],
+            'expectation' => interaction[:expectation],
           }
         end
-        mapped_groups.unshift(
+      end
+
+      def prepare_searches(searches, resource_name)
+        searches.map.with_index do |search, index|
           {
-            'title' => "CapabilityStatement",
-            'resource' => "CapabilityStatement",
-            'position' => 1,
-            'profile_url' => "CapabilityStatement",
-            'description' => Helpers.get_capability_statement_group_description_text('Capability Statement', false),
-            'interactions' => [],
-            'searches' => []
+            'names' => search[:names],
+            'expectation' => search[:expectation],
+            'search_string' => make_search_string(resource_name, search[:names]),
+            'position' => index + 1
           }
-        )
-        mapped_groups
+        end
+      end
+
+      def prepare_group_description(group)
+        required_searches = group.searches.select { |search| search[:expectation] == 'SHALL' }
+        search_param_name_string = required_searches
+                                     .map { |search| search[:names].join(' + ') }
+                                     .map { |names| "* #{names}" }
+                                     .join("\n")
+        Helpers.get_group_description_text(group.title,
+                                           group.resource,
+                                           group.profile_name,
+                                           group.version,
+                                           group.profile_url,
+                                           required_searches,
+                                           search_param_name_string,
+                                           "#{group.resource} resources",
+                                           false)
+      end
+
+      def map_group(group)
+        {
+          'title' => group.title,
+          'resource' => group.resource,
+          'profile_url' => group.profile_url,
+          'description' => prepare_group_description(group),
+          'interactions' => prepare_interactions(group.interactions),
+          'searches' => prepare_searches(group.searches, group.resource)
+        }
+      end
+
+      def capability_statement_group
+        {
+          'title' => "CapabilityStatement",
+          'resource' => "CapabilityStatement",
+          'profile_url' => "CapabilityStatement",
+          'description' => Helpers.get_capability_statement_group_description_text('Capability Statement', false),
+          'interactions' => [],
+          'searches' => []
+        }
+      end
+
+      def resource_list
+        filtered_profile_groups = ig_metadata.ordered_groups.reject { |group| EXCLUDED_RESOURCES.include? group.resource }
+        all_profiles_groups = [capability_statement_group] + filtered_profile_groups.map { |group| map_group(group) }
+        all_profiles_groups.map.with_index(1) {|hash, index| hash.merge({"position" => index}) }
       end
 
       def make_search_string(resource_name, search_names)
