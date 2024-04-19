9c57deb5ef844b7878b528c3bcb580bda7ca87c4
Pavel R <prozskov@gmail.com>
2024-04-19 17:20:57 +0200

    Fix a potential security problem

--- lib/au_core_test_kit/generator/ig_download.rb
+++ lib/au_core_test_kit/generator/ig_download.rb
@@ -1,51 +1,59 @@
 # frozen_string_literal: true
 
 require 'nokogiri'
-require 'open-uri'
-require 'uri'
 require 'json'
 require 'net/http'
 
 url = 'http://hl7.org.au/fhir/core/history.html'
 
-html_content = URI.open(url).read
+begin
+  uri = URI.parse(url)
+  response = Net::HTTP.get_response(uri)
 
-doc = Nokogiri::HTML(html_content)
+  unless response.is_a?(Net::HTTPSuccess)
+    puts "Failed to fetch HTML content from #{url}. HTTP Status: #{response.code} #{response.message}"
+    exit
+  end
 
-script_tag = doc.at('script:contains("var pageJSON")')
+  html_content = response.body
 
-ig_versions_to_skip = ['0.1.0-draft', '0.2.0-preview', '0.2.1-preview', '0.2.2-preview']
+  doc = Nokogiri::HTML(html_content)
 
-if script_tag&.content
-  json_data = script_tag.content[/\{.*\}/m]
-  if json_data
-    data_hash = JSON.parse(json_data)
-    data_hash['list'].each do |item|
-      version = item['version']
-      next if ig_versions_to_skip.include? version
+  script_tag = doc.at('script:contains("var pageJSON")')
 
-      package_url = "#{item['path']}/package.tgz"
-      file_name = "#{version}.tgz"
-      output_directory = 'lib/au_core_test_kit/igs'
+  ig_versions_to_skip = ['0.1.0-draft', '0.2.0-preview', '0.2.1-preview', '0.2.2-preview']
 
-      Dir.mkdir(output_directory) unless File.directory?(output_directory)
+  if script_tag&.content
+    json_data = script_tag.content[/\{.*\}/m]
+    if json_data
+      data_hash = JSON.parse(json_data)
+      data_hash['list'].each do |item|
+        version = item['version']
+        next if ig_versions_to_skip.include? version
 
-      File.open(File.join(output_directory, file_name), 'wb') do |file|
-        uri = URI.parse(package_url)
-        response = Net::HTTP.get_response(uri)
-        if response.is_a?(Net::HTTPSuccess)
-          file.write(response.body)
-          puts "Successfully downloaded #{file_name}"
-        else
-          puts "Failed to download #{file_name}. HTTP Status: #{response.code} #{response.message}"
+        package_url = "#{item['path']}/package.tgz"
+        file_name = "#{version}.tgz"
+        output_directory = 'lib/au_core_test_kit/igs'
+
+        Dir.mkdir(output_directory) unless File.directory?(output_directory)
+
+        File.open(File.join(output_directory, file_name), 'wb') do |file|
+          uri = URI.parse(package_url)
+          response = Net::HTTP.get_response(uri)
+          if response.is_a?(Net::HTTPSuccess)
+            file.write(response.body)
+            puts "Successfully downloaded #{file_name}"
+          else
+            puts "Failed to download #{file_name}. HTTP Status: #{response.code} #{response.message}"
+          end
         end
-      rescue StandardError => e
-        puts "An error occurred while downloading #{file_name}: #{e.message}"
       end
+    else
+      puts 'Error: Unable to extract JSON data from script tag.'
     end
   else
-    puts 'Error: Unable to extract JSON data from script tag.'
+    puts 'Error: Script tag containing pageJSON variable not found.'
   end
-else
-  puts 'Error: Script tag containing pageJSON variable not found.'
+rescue StandardError => e
+  puts "An error occurred: #{e.message}"
 end
