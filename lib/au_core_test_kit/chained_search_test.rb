9c6abfcc267a379c82e791e05ed547701a9faddf
Pavel R <prozskov@gmail.com>
2024-05-09 11:36:20 +0200

    Code optimisation and cleanup Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/46

--- lib/au_core_test_kit/chained_search_test.rb
+++ lib/au_core_test_kit/chained_search_test.rb
@@ -1,48 +1,32 @@
 # frozen_string_literal: true
 
-require_relative 'date_search_validation'
-require_relative 'fhir_resource_navigation'
 require_relative 'search_test_properties'
 
 module AUCoreTestKit
   module ChainedSearchTest
     extend Forwardable
-    include DateSearchValidation
-    include FHIRResourceNavigation
 
     def_delegators 'self.class', :metadata, :provenance_metadata, :properties
     def_delegators 'properties',
                    :resource_type,
                    :search_param_names
 
-    def any_valid_search_params?(search_params)
-      search_params.any? { |_patient_id, params| params.present? }
-    end
-
     def extract_target_resource_from_chained_search_parameter(search_param)
       search_param.split(':').second.split('.').first
     end
 
     def get_resources_identifier(resources)
-      result = []
-
-      resources.each do |r|
-        r.identifier.each do |ident|
-          result << ident.value
-        end
-      end
-
-      result
+      resources.map do |r|
+        r.identifier.map(&:value)
+      end.flatten
     end
 
     def all_chain_identifier_values(patient_id_list, all_resources, chain_target)
-      result = []
-      patient_id_list.each do |patient_id|
-        all_patient_resources = all_resources[patient_id]
-        target_resources = all_patient_resources.filter { |r| r.resourceType == chain_target }
-        result << get_resources_identifier(target_resources)
-      end
-      result.flatten
+      patient_id_list.map do |patient_id|
+        get_resources_identifier(
+          all_resources[patient_id].filter { |r| r.resourceType == chain_target }
+        )
+      end.flatten
     end
 
     def run_chain_search_test
@@ -61,9 +45,7 @@ module AUCoreTestKit
 
       skip_if all_values.empty?, "I don't have values to perform search"
 
-      all_values.each do |val|
-        search_and_check_response({ search_param => val })
-      end
+      search_and_check_response({ search_param => all_values.sample })
     end
 
     def search_and_check_response(params, resource_type = self.resource_type)
