4b61a63aa661d112828e8d9b2f356a8063d93e86
Pavel R <prozskov@gmail.com>
2024-05-16 12:22:46 +0200

    Class cleanup Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/80

--- lib/au_core_test_kit/generator/special_identifier_search_test_generator.rb
+++ lib/au_core_test_kit/generator/special_identifier_search_test_generator.rb
@@ -2,10 +2,11 @@
 
 require_relative 'naming'
 require_relative 'special_cases'
+require_relative 'search_test_generator'
 
 module AUCoreTestKit
   class Generator
-    class SpecialIdentifierSearchTestGenerator
+    class SpecialIdentifierSearchTestGenerator < SearchTestGenerator
       class << self
         def generate(ig_metadata, base_output_dir)
           ig_metadata.groups.reject { |group| SpecialCases.exclude_group? group }
@@ -26,9 +27,7 @@ module AUCoreTestKit
       attr_accessor :group_metadata, :search_metadata, :base_output_dir, :special_identifier
 
       def initialize(group_metadata, search_metadata, base_output_dir, special_identifier)
-        self.group_metadata = group_metadata
-        self.search_metadata = search_metadata
-        self.base_output_dir = base_output_dir
+        super(group_metadata, search_metadata, base_output_dir)
         self.special_identifier = special_identifier
       end
 
@@ -36,94 +35,16 @@ module AUCoreTestKit
         @template ||= File.read(File.join(__dir__, 'templates', 'special_identifier_search.rb.erb'))
       end
 
-      def output
-        @output ||= ERB.new(template).result(binding)
-      end
-
-      def base_output_file_name
-        "#{class_name.underscore}.rb"
-      end
-
-      def output_file_directory
-        File.join(base_output_dir, profile_identifier)
-      end
-
-      def output_file_name
-        File.join(output_file_directory, base_output_file_name)
-      end
-
-      def profile_identifier
-        Naming.snake_case_for_profile(group_metadata)
-      end
-
       def test_id
         "au_core_#{group_metadata.reformatted_version}_#{profile_identifier}_#{search_identifier}_#{special_identifier}_search_test"
-      end
-
-      def search_identifier
-        search_metadata[:names].join('_').tr('-', '_')
-      end
-
-      def search_title
-        search_identifier.camelize
       end
 
       def class_name
         "#{Naming.upper_camel_case_for_profile(group_metadata)}#{search_title}_#{special_identifier}SearchTest"
       end
 
-      def module_name
-        "AUCore#{group_metadata.reformatted_version.upcase}"
-      end
-
-      def resource_type
-        group_metadata.resource
-      end
-
-      def search_params
-        @search_params ||=
-          search_metadata[:names].map do |name|
-            {
-              name:,
-              path: search_definition(name)[:path]
-            }
-          end
-      end
-
-      def search_param_name_string
-        search_metadata[:names].join(' + ')
-      end
-
-      def search_param_names
-        search_params.map { |param| param[:name] }
-      end
-
-      def search_param_names_array
-        array_of_strings(search_param_names)
-      end
-
       def optional?
         true
-      end
-
-      def search_definition(name)
-        group_metadata.search_definitions[name.to_sym]
-      end
-
-      def token_search_params
-        @token_search_params ||=
-          search_param_names.select do |name|
-            %w[Identifier CodeableConcept Coding].include? group_metadata.search_definitions[name.to_sym][:type]
-          end
-      end
-
-      def token_search_params_string
-        array_of_strings(token_search_params)
-      end
-
-      def array_of_strings(array)
-        quoted_strings = array.map { |element| "'#{element}'" }
-        "[#{quoted_strings.join(', ')}]"
       end
 
       def search_properties
@@ -133,29 +54,6 @@ module AUCoreTestKit
           properties[:token_search_params] = token_search_params_string if token_search_params.present?
           properties[:identifier_type] = "'#{special_identifier}'"
         end
-      end
-
-      def url_version
-        case group_metadata.version
-        when 'v0.3.0-ballot'
-          '0.3.0-ballot'
-        end
-      end
-
-      def search_test_properties_string
-        search_properties
-          .map { |key, value| "#{' ' * 8}#{key}: #{value}" }
-          .join(",\n")
-      end
-
-      def generate
-        FileUtils.mkdir_p(output_file_directory)
-        File.open(output_file_name, 'w') { |f| f.write(output) }
-
-        group_metadata.add_test(
-          id: test_id,
-          file_name: base_output_file_name
-        )
       end
 
       def title
