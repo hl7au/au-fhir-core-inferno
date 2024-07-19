3ee24783a17eb1eb440900dec3b10927f993a9dd
nvim <prozskov@gmail.com>
2024-07-19 08:06:53 +0200

    Use gem as validation test
    Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/165

--- lib/au_core_test_kit.rb
+++ lib/au_core_test_kit.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'validation_test_kit'
+
 require_relative 'au_core_test_kit/generated/v0.3.0-ballot/au_core_test_suite'
 
 require_relative 'au_core_test_kit/generated/v0.4.0-preview/au_core_test_suite'
 
 # require_relative 'au_core_test_kit/generated/v0.5.0-ci-build/au_core_test_suite'
+
+# require_relative 'au_core_tet_kit/custom_suites/custom_validation_suite'
