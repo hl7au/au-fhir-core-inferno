152c3c9ffd6d57a4e38281c3f58caaa84fd3b2cf
Kyle Pettigrew <kylepettigrew@gmail.com>
2024-07-23 11:51:17 +1000

    rds impl

--- infra/aws-impl/data.tf
+++ infra/aws-impl/data.tf
@@ -7,3 +7,17 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster_auth" {
   name = var.cluster_name
 }
+
+
+
+data "aws_vpc" "k8s" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+
+  }
+}
+
+data "aws_db_subnet_group" "k8s" {
+  name = var.vpc_name
+}
--- infra/aws-impl/variables.tf
+++ infra/aws-impl/variables.tf
@@ -8,3 +8,10 @@ variable "cluster_name" {
   type    = string
   default = "fhir-k8s-dev"
 }
+
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "fhir-k8s-dev-vpc"
+}
