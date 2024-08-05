391b7387a89186c2ed9c2ed65d7176cc59d78d30
nvim <prozskov@gmail.com>
2024-08-05 18:07:49 +0200

    Comment 1.0.0

--- lib/au_core_test_kit.rb
+++ lib/au_core_test_kit.rb
@@ -8,4 +8,4 @@ require_relative 'au_core_test_kit/custom_suites/validation_suite'
 
 # require_relative 'au_core_test_kit/generated/v0.5.0-ci-build/au_core_test_suite'
 
-require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
+# require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
