edb4af7baba189cb921d20613cf2724334ef4e2b
nvim <prozskov@gmail.com>
2024-08-09 09:27:26 +0200

    Remove 0.3.0-ballot, 0.4.1-preview and add 1.0.0-ballot
    Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/187

--- lib/au_core_test_kit.rb
+++ lib/au_core_test_kit.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require_relative 'au_core_test_kit/generated/v0.3.0-ballot/au_core_test_suite'
+# require_relative 'au_core_test_kit/generated/v0.3.0-ballot/au_core_test_suite'
 
-require_relative 'au_core_test_kit/generated/v0.4.1-preview/au_core_test_suite'
-
-require_relative 'au_core_test_kit/custom_suites/validation_suite'
+# require_relative 'au_core_test_kit/generated/v0.4.1-preview/au_core_test_suite'
 
 # require_relative 'au_core_test_kit/generated/v0.5.0-ci-build/au_core_test_suite'
 
-# require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
+require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
+
+require_relative 'au_core_test_kit/custom_suites/validation_suite'
