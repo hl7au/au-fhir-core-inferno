3da683780a180521879d3341f8aea4137d209c0b
Kyle Pettigrew <kylepettigrew@gmail.com>
2024-11-04 09:03:48 +1000

    tf fmt

--- infra/aws-impl/main.tf
+++ infra/aws-impl/main.tf
@@ -79,8 +79,8 @@ module "rds" {
   performance_insights_enabled          = true
   performance_insights_retention_period = 7
 
-  tags = local.tags
-  manage_master_user_password_rotation = true
+  tags                        = local.tags
+  manage_master_user_password = true
 }
 
 ################################################################################
