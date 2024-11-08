ac422a4e69b4911b7cfe08844c7801b6f95060f2
nvim <prozskov@gmail.com>
2024-11-08 11:19:15 +0100

    Remove current IG from ignore list

--- lib/au_core_test_kit.rb
+++ lib/au_core_test_kit.rb
@@ -9,5 +9,3 @@
 require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
 
 require_relative 'au_core_test_kit/custom_suites/validation_suite'
-
-# require_relative 'au_core_test_kit/generated/v1.0.0-ci-build/au_core_test_suite'
