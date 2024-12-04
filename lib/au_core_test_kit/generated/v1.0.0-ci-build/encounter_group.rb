3c6627eb7a717b20113a089c281cccafb700a902
projkov <4172228+projkov@users.noreply.github.com>
2024-12-04 08:31:12 +0000

    Merge new auto generated tests

--- lib/au_core_test_kit/generated/v1.0.0-ci-build/encounter_group.rb
+++ lib/au_core_test_kit/generated/v1.0.0-ci-build/encounter_group.rb
@@ -9,6 +9,7 @@ require_relative 'encounter/encounter_date_patient_search_test'
 require_relative 'encounter/encounter_class_patient_search_test'
 require_relative 'encounter/encounter_patient_location_search_test'
 require_relative 'encounter/encounter_patient_status_search_test'
+require_relative 'encounter/encounter_status_multiple_or_search_test'
 require_relative 'encounter/encounter_date_multiple_and_search_test'
 require_relative 'encounter/encounter_patient_chain_search_test'
 require_relative 'encounter/encounter_patient_ihi_chain_search_test'
@@ -94,6 +95,7 @@ read succeeds.
       test from: :au_core_v100_ci_build_encounter_class_patient_search_test
       test from: :au_core_v100_ci_build_encounter_patient_location_search_test
       test from: :au_core_v100_ci_build_encounter_patient_status_search_test
+      test from: :au_core_v100_ci_build_encounter_status_multiple_or_search_test
       test from: :au_core_v100_ci_build_encounter_date_multiple_and_search_test
       test from: :au_core_v100_ci_build_encounter_patient_chain_search_test
       test from: :au_core_v100_ci_build_encounter_patient_ihi_chain_search_test
