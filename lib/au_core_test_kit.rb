a5506331e241884b54c5b5fbfdbca06ab51c0fda
nvim <prozskov@gmail.com>
2024-12-05 08:04:21 +0100

    Use 1.0.0-preview as single available version

--- lib/au_core_test_kit.rb
+++ lib/au_core_test_kit.rb
@@ -6,10 +6,10 @@
 
 # require_relative 'au_core_test_kit/generated/v0.5.0-ci-build/au_core_test_suite'
 
-require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
+# require_relative 'au_core_test_kit/generated/v1.0.0-ballot/au_core_test_suite'
 
 require_relative 'au_core_test_kit/custom_suites/validation_suite'
 
-require_relative 'au_core_test_kit/generated/v1.0.0-ci-build/au_core_test_suite'
+# require_relative 'au_core_test_kit/generated/v1.0.0-ci-build/au_core_test_suite'
 
 require_relative 'au_core_test_kit/generated/v1.0.0-preview/au_core_test_suite'
