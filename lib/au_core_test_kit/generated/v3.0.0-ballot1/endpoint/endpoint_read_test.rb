8bbfc7022bc2df52842c95a8c2810d098e2385d8
Pavel Rozhkov <prozskov@gmail.com>
2026-08-04 12:49:56 +0400

    Generate the Inferno Suite for the AU Core v3.0.0-ballot1
    Ref: https://github.com/hl7au/au-fhir-core-inferno/issues/300

---
+++ lib/au_core_test_kit/generated/v3.0.0-ballot1/endpoint/endpoint_read_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'inferno_suite_generator/test_modules/read_test'
+
+module AUCoreTestKit
+  module AUCoreV300_BALLOT1
+    class EndpointReadTest < Inferno::Test
+      include InfernoSuiteGenerator::ReadTest
+
+      title '(SHALL) Server returns correct Endpoint resource from Endpoint read interaction'
+      description 'A server SHALL support the Endpoint read interaction.'
+
+      input :endpoint_ids,
+            title: 'Endpoint IDs',
+            description: 'Comma separated list of endpoint IDs that in sum contain all MUST SUPPORT elements',
+            default: 'bobrester-fhir-rest, mh-audiology-smd, murrabit-hospital-hl7-v2-mllp',
+            optional: true
+
+      id :au_core_v300_ballot1_endpoint_read_test
+
+      def self.demodata
+        @demodata ||= InfernoSuiteGenerator::Generator::IGDemodata.new(
+          YAML.load_file(File.join(File.dirname(__dir__), 'demodata.yml'), aliases: true)
+        )
+      end
+
+      def resource_type
+        'Endpoint'
+      end
+
+      def scratch_resources
+        scratch[:endpoint_resources] ||= {}
+      end
+
+      run do
+        perform_read_test(scratch.dig(:references, 'Endpoint'))
+      end
+    end
+  end
+end
---
+++ lib/au_core_test_kit/generated/v3.0.0-ballot1/patient/patient_identifier_dva_search_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative '../../../generators/custom_identifier_search/special_identifier_search_test'
+
+module AUCoreTestKit
+  module AUCoreV300_BALLOT1
+    class PatientIdentifierDVASearchTest < InfernoSuiteGenerator::SpecialIdentifierSearchTest
+      title '(SHOULD) Server returns valid results for Patient search by identifier (DVA)'
+      description %(A server SHOULD support searching by
+identifier (DVA) on the Patient resource. This test
+will pass if resources are returned and match the search criteria. If
+none are returned, the test is skipped.
+
+[AU Core Server CapabilityStatement](http://hl7.org.au/fhir/core/3.0.0-ballot1/CapabilityStatement-au-core-server.html)
+)
+
+      id :au_core_v300_ballot1_patient_identifier_dva_search_test
+      optional
+
+      def self.properties
+        @properties ||= SearchTestProperties.new(
+          resource_type: 'Patient',
+          search_param_names: ['identifier'],
+          token_search_params: ['identifier'],
+          target_identifier: { display: 'DVA', url: 'http://ns.electronichealth.net.au/id/dva' }
+        )
+      end
+
+      def self.metadata
+        @metadata ||= InfernoSuiteGenerator::Generator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true))
+      end
+
+      def scratch_resources
+        scratch[:patient_resources] ||= {}
+      end
+
+      run do
+        run_special_identifier_search_test
+      end
+    end
+  end
+end
